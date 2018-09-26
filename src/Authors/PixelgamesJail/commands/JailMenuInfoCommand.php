9d59defe25ae7c35a454e1687e8c73c19019643b
iStrafeNubzHDyt <pixelgameskontakt@gmail.com>
2018-09-26 23:27:29 +0200

    forgot this...

--- src/Authors/PixelgamesJail/commands/JailMenuInfoCommand.php
+++ src/Authors/PixelgamesJail/commands/JailMenuInfoCommand.php
@@ -15,7 +15,7 @@ class JailMenuInfoCommand extends BaseCommand
             $issuer->sendMessage("§ePlugin von hoyinm14mc, iStrafeNubzHDyt");
             $issuer->sendMessage("§bName: PixelgamesJail");
             $issuer->sendMessage("§bOriginal: Jail");
-            $issuer->sendMessage("§bVersion: 2.0.1#");
+            $issuer->sendMessage("§bVersion: 2.1.1#");
             $issuer->sendMessage("§bFür PocketMine-API: 3.0.0, 4.0.0");
             $issuer->sendMessage("§6Permissions: pgjail.override.restrictions, pgjail.command.setjail, pgjail.command.deljail, pgjail.command.jails, pgjail.command.jail, pgjail.command.unjail, pgjail.command.jailed, pgjail.command.switchjail, pgjail.command.jailtp, pgjail.command.bail, pgjail.command.jailmine, pgjail.command.votejail, pgjail.uuidcheck.bypass, pgjail.sign.destroy, pgjail.sign.create, pgjail.sign.use, pgjail.modify.bypass, pgjail.command.jailsellhand, pgjail.command.jailresetmine, pgjail.command.jailinfo, pgjail.command.prisonerinfo, pgjail.showInOutMessage, pgjail.visit.bypass, pgjail.command.jail.info, pgjail.command.jail.help");
             $issuer->sendMessage("§eSpeziell für PIXELGAMES entwickelt");
